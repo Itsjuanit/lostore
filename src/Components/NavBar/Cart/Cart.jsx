16dbc39b8f2c830e3b623279ab32f51c0fd9e443
Juan Ignacio Tejada <jtejada@lubee.com.ar>
2022-05-02 20:28:08 -0300

    Cambios en el css

--- src/Components/NavBar/Cart/Cart.jsx
+++ src/Components/NavBar/Cart/Cart.jsx
@@ -1,7 +1,9 @@
 import { FaCartPlus } from "react-icons/fa";
 
 function Cart() {
-  return <FaCartPlus size={"2em"} color={"#ecc08b"} />;
+  return (
+    <FaCartPlus size={"2em"} color={"#ecc08b"} style={{ cursor: "pointer" }} />
+  );
 }
 
 export default Cart;
